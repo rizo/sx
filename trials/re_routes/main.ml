1f3afae47f9ce30c16790bd8c78e442ae5f36a40
Rizo <rizo@odis.io>
2025-04-17 10:55:25 +0100

    Rematch

--- trials/re_routes/main.ml
+++ trials/re_routes/main.ml
@@ -7,20 +7,37 @@ module Common = struct
   (** -?[0-9]+ *)
   let int = seq [ opt (char '-'); pos_int ]
 
-  (** -?[0-9]+( .[0-9]* )? *)
-  let float =
-    seq [ opt (char '-'); rep1 digit; opt (seq [ char '.'; rep digit ]) ]
+  (** -?[0-9]+\.[0-9]* *)
+  let float = seq [ opt (char '-'); rep1 digit; seq [ char '.'; rep digit ] ]
 
   (** true|false *)
   let bool = alt [ str "true"; str "false" ]
 end
 
-module Rematch = struct
+module Rematch : sig
+  type 'a t
+
+  val compile : (Re.t * (Re.Group.t -> 'a)) list -> 'a t
+  (** [compile cases] produces a rematch state from pairs [(re, action)] in
+      [cases].
+
+      The resulting state marks each [re] so that when it is matched, [action]
+      is called with the matched group.
+
+      @raise Invalid_argument when [cases] is empty. *)
+
+  val exec : 'a t -> string -> 'a option
+  (** [exec rematch input] attempts to match [input] with cases in [rematch],
+      producing the result of calling the action associated to matched regular
+      expression. *)
+end = struct
   module Mark_map = Map.Make (Re.Mark)
 
-  let cases xs =
-    match xs with
-    | (re0, k0) :: res ->
+  type 'a t = (Re.Group.t -> 'a) Mark_map.t * Re.re
+
+  let compile cases0 =
+    match cases0 with
+    | (re0, k0) :: cases' ->
       let m0, re0' = Re.mark re0 in
       let ctx0 = Mark_map.singleton m0 k0 in
       let ctx, re =
@@ -30,37 +47,42 @@ module Rematch = struct
             let ctx' = Mark_map.add m k ctx in
             let acc' = Re.alt [ acc; re' ] in
             (ctx', acc'))
-          (ctx0, re0') res
+          (ctx0, re0') cases'
       in
       (ctx, Re.compile re)
-    | _ -> failwith "insufficient routes"
+    | [] -> invalid_arg "empty cases"
 
   let exec (ctx, re) input =
     match Re.exec_opt re input with
-    | None -> failwith "no match"
+    | None -> None
     | Some g ->
-      let ml = Re.Mark.all g in
-      Re.Mark.Set.iter
-        (fun m ->
-          let k = Mark_map.find m ctx in
-          Printf.printf "k = %S\n" k)
-        ml
+      let marks = Re.Mark.all g in
+      assert (Re.Mark.Set.cardinal marks = 1);
+      let m = Re.Mark.Set.min_elt marks in
+      let k = Mark_map.find m ctx in
+      Some (k g)
 end
 
+open Printf
+
 let r1 =
-  Rematch.cases
+  Rematch.compile
     [
-      (Common.pos_int, "POSITIVE");
-      (Common.int, "INTEGER");
-      (Common.float, "FLOATING");
-      (Common.bool, "BOOLEAN");
+      (Common.float, fun g -> sprintf "FLOATING: %S\n" (Re.Group.get g 0));
+      (Common.pos_int, fun g -> sprintf "POSITIVE: %S\n" (Re.Group.get g 0));
+      (Common.int, fun g -> sprintf "INTEGER: %S\n" (Re.Group.get g 0));
+      (Common.bool, fun g -> sprintf "BOOLEAN: %S\n" (Re.Group.get g 0));
     ]
+
+(* FIXME: abc123 should not match int *)
 
 let () =
   while true do
-    print_string "> ";
-    flush stdout;
+    printf "> %!";
     match In_channel.input_line stdin with
     | None -> ()
-    | Some line -> begin Rematch.exec r1 line end
+    | Some line -> (
+      match Rematch.exec r1 line with
+      | Some value -> printf "%s\n%!" value
+      | None -> printf "no match!\n%!")
   done
